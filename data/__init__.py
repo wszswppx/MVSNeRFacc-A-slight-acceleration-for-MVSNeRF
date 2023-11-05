4c5e448c27f612e1e13e608b335092415e473013
Xiaotao <32038518+wszswppx@users.noreply.github.com>
2023-11-05 14:39:07 +0800

    Delete data directory

--- data/__init__.py
+++
@@ -1,9 +0,0 @@
-from .llff import LLFFDataset
-from .blender import BlenderDataset
-from .dtu_ft import DTU_ft
-from .dtu import MVSDatasetDTU
-
-dataset_dict = {'dtu': MVSDatasetDTU,
-                'llff':LLFFDataset,
-                'blender': BlenderDataset,
-                'dtu_ft': DTU_ft}
